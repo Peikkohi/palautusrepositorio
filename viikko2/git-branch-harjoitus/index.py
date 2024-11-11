d0fea371a9aed6bdb3dc78af4e607de7faff33bf
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-11 23:48:53 +0200

    run summa & erotus in main

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,8 +1,12 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku I: "))
 y = int(input("luku II: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
