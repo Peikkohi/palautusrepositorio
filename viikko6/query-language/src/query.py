55b92cc11492961f6eb40150874dc454a2c98326
Peikkohi <26819965+Peikkohi@users.noreply.github.com>
2024-12-09 20:15:18 +0200

    lisää vaihtoehtoinen esimerkki

--- viikko6/query-language/src/query.py
+++ viikko6/query-language/src/query.py
@@ -7,6 +7,48 @@ from matchers import (
     Or
 )
 
+# class Query:
+#     def __init__(self, predicate=lambda _: True):
+#         self.predicate = predicate
+# 
+#     @staticmethod
+#     def _conjunction(predicate_a, predicate_b):
+#         return lambda player: predicate_a(player) and predicate_b(player)
+# 
+#     def _append(self, predicate):
+#         return Query(self._conjunction(predicate, self.predicate))
+# 
+#     @staticmethod
+#     def _has_at_least(value, attr):
+#         return lambda player: getattr(player, attr) >= value
+# 
+#     def has_at_least(self, value, attr):
+#         return self._append(self._has_at_least(value, attr))
+# 
+#     @staticmethod
+#     def _plays_in(team):
+#         return lambda player: player.team == team
+# 
+#     def plays_in(self, team):
+#         return self._append(self._plays_in(team))
+# 
+#     @staticmethod
+#     def _negate(predicate):
+#         return lambda player: not predicate(player)
+# 
+#     def has_fewer_than(self, value, attr):
+#         return self._append(self._negate(self._has_at_least))
+# 
+#     @staticmethod
+#     def _one_of(*predicates):
+#         return lambda player: any(predicate(player) for predicate in predicates)
+# 
+#     def one_of(self, *predicates):
+#         return self._append(self._one_of(*predicates))
+# 
+#     def build(self):
+#         return self.predicate
+
 class Query:
     "builds a query for selecting players based on Conjuctive normal form"
     def __init__(self, predicate=All()):
